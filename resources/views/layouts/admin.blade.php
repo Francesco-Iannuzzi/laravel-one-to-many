2ca6758dc68e8705c744c5b788b8c6f3293bf763
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-06-07 16:03:09 +0200

    implement logout in admin.blade and fix height

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -24,22 +24,40 @@
 
 <body>
     <div id="app" class="vh-100">
-        <nav class="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow">
-            <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
+        <nav id="navbar_admin" class="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow">
+
+            <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Portfolio</a>
+
             <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse"
                 data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
             <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-            <ul class="navbar-nav px-3">
-                <li class="nav-item text-nowrap">
-                    <a class="nav-link" href="#">Sign out</a>
-                </li>
-            </ul>
+
+            <div class="dropstart">
+                <button class="btn dropdown-toggle text-light" type="button" id="triggerId" data-bs-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    {{ Auth::user()->name }}
+                </button>
+                <div class="dropdown-menu dropdown-menu-dark" aria-labelledby="triggerId">
+                    <a class="dropdown-item" href="{{ url('/') }}">{{ __('Home Page') }}</a>
+                    <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </div>
+
         </nav>
 
-        <div class="container-fluid h-100">
+        <div id="container_admin" class="container-fluid">
             <div class="row h-100">
 
                 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
