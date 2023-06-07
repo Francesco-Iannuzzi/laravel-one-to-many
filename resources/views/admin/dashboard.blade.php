7f1244127fdae9451316e0784f8be5ffdd101ab4
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-06-07 16:03:33 +0200

    add container to content admin.blade

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
+    <div class="container py-5">
         <h2 class="fs-4 text-secondary my-4">
             {{ __('Dashboard') }}
         </h2>
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,29 +1,31 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="card mt-5 mb-3">
-        <div class="row g-0">
-            <div class="col-md-4">
-                <img src="{{ $project->cover }}" class="img-fluid rounded" alt="{{ $project->title }}">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <div class="card-top">
-                        <h2 class="card-title">{{ $project->title }}</h2>
-                        <h5><strong>Author: </strong>{{ $project->made_by }}</h5>
-                        <small><strong>Date: </strong>{{ $project->creation_date }}</small>
-                    </div>
-                    <div class="card-bottom">
-                        <p class="card-text"><strong>Description: </strong>{{ $project->description }}</p>
-                        <p class="card-text"><strong>Trace: </strong>{{ $project->trace }}</p>
-                        <div class="btn-group">
-                            <button class="btn btn-primary dropdown-toggle" type="button" id="triggerId"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Links
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-start" aria-labelledby="triggerId">
-                                <a class="dropdown-item" href="{{ $project->link }}">Link</a>
-                                <a class="dropdown-item" href="{{ $project->code_link }}">Code's Link</a>
+    <div class="container">
+        <div class="card mt-5 mb-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="{{ $project->cover }}" class="img-fluid rounded" alt="{{ $project->title }}">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <div class="card-top">
+                            <h2 class="card-title">{{ $project->title }}</h2>
+                            <h5><strong>Author: </strong>{{ $project->made_by }}</h5>
+                            <small><strong>Date: </strong>{{ $project->creation_date }}</small>
+                        </div>
+                        <div class="card-bottom">
+                            <p class="card-text"><strong>Description: </strong>{{ $project->description }}</p>
+                            <p class="card-text"><strong>Trace: </strong>{{ $project->trace }}</p>
+                            <div class="btn-group">
+                                <button class="btn btn-primary dropdown-toggle" type="button" id="triggerId"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Links
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-start" aria-labelledby="triggerId">
+                                    <a class="dropdown-item" href="{{ $project->link }}">Link</a>
+                                    <a class="dropdown-item" href="{{ $project->code_link }}">Code's Link</a>
+                                </div>
                             </div>
                         </div>
                     </div>
