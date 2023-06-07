551ba0fa416ea1a8b58153a905d35c62dca56f01
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-06-07 17:27:56 +0200

    add relation in model Type and Project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,16 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
 
 class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'made_by', 'slug', 'description', 'creation_date', 'cover', 'link', 'code_link', 'trace'];
+    protected $fillable = ['title', 'made_by', 'slug', 'description', 'creation_date', 'cover', 'link', 'code_link', 'trace', 'type_id'];
 
     public static function generateSlug($title)
     {
         return Str::slug($title, '-');
+    }
+
+    /**
+     * Get the user that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function types(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
     }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class Type extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+
+    public static function generateSlug($name)
+    {
+        return Str::slug($name, '-');
+    }
+
+    /**
+     * Get all of the comments for the Type
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
 }
